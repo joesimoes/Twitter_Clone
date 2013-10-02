81d553a1c34029639166b7225385e3fed1435111
Joseph Simoes <joseph.simoes@generalassemb.ly>
2013-10-02 19:24:56 -0400

    Add Form and Ribbit Submissions to User Show Page

--- app/controllers/ribbits_controller.rb
+++ app/controllers/ribbits_controller.rb
@@ -1,2 +1,10 @@
 class RibbitsController < ApplicationController
+
+  def create
+    ribbit = Ribbit.new(params[:ribbit])
+    ribbit.user_id = current_user.id
+
+    flash[:error] = "Your Ribbit was over 140 characters" unless ribbit.save
+    redirect_to root_url
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,5 +15,6 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-  end
+    @ribbit = Ribbit.new
+   end
 end
