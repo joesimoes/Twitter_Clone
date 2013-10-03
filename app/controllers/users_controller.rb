79029689e92745b7b563227fc9a11dd52a650c8f
Joseph Simoes <joseph.simoes@generalassemb.ly>
2013-10-03 15:42:52 -0400

    Create buddies page and profile editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 
   def new
-    @user = User.new
+    if current_user
+      redirect_to buddies_path      
+    else
+      @user = User.new
+    end
   end
 
   def create
@@ -14,8 +18,41 @@ class UsersController < ApplicationController
   	end
   end
   
+  def index
+    @users = User.all
+    @ribbit = Ribbit.new
+  end
+
+  def buddies
+    if current_user
+      @ribbit = Ribbit.new
+      buddies_ids = current_user.followeds.map(&:id).push(current_user.id)
+      @ribbits = Ribbit.find_all_by_user_id buddies_ids
+    else
+      redirect_to root_url
+    end
+  end
+
   def show
     @user = User.find(params[:id])
     @ribbit = Ribbit.new
+    @relationship = Relationship.where(
+      follower_id: current_user.id,
+      follower_id: @user.id
+    ).first_or_initialize if current_user
    end
+
+  def edit
+    @user = User.find(params[:id]) 
+    redirect_to @user unless @user == current_user
+  end
+
+  def update
+    @user = User.find(params[:id]) 
+    if @user.update_attributes(params[:user])
+      redirect_to @user, notice: "Profile Updated"
+    else
+      render 'edit'
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,12 @@ class User < ActiveRecord::Base
  
 
   has_many :ribbits
- 
+  has_many :follower_relationships, class_name: 'Relationship', foreign_key: 'followed_id'
+  has_many :followed_relationships, class_name: 'Relationship', foreign_key: 'follower_id'
+  has_many :followers, through: :follower_relationships
+  has_many :followeds, through: :followed_relationships
+
+  
   before_validation :prep_email
   before_save :create_avatar_url
     
@@ -14,6 +19,14 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true, format:{ with: /^[\w\.+-]+@([\w]+\.)+[a-zA-Z]/ }
   validates :username, presence: true, uniqueness: true
   validates :name, presence: true
+
+  def following? user
+    self.followeds.include? user
+  end
+  
+  def follow user
+    Relationship.create follower_id: self.id, followed_id: user.id
+  end
 
   private
   
